e3b4ae93a998628dc51fa26b58405b5de38230c6
Ludovic Rousseau <ludovic.rousseau+github@gmail.com>
2012-05-22 16:02:09 +0200

    hid_enumerate() can match any vendor or any product
    It is now possible to use product_id=0 to match any product of a given
    vendor.
    It is also possible to use vendor_id=0 to match any vendor for a given
    product but that may not be used often.

--- linux/hid.c
+++ linux/hid.c
@@ -431,8 +431,8 @@ struct hid_device_info  HID_API_EXPORT *hid_enumerate(unsigned short vendor_id, 
 		}
 
 		/* Check the VID/PID against the arguments */
-		if ((vendor_id == 0x0 && product_id == 0x0) ||
-		    (vendor_id == dev_vid && product_id == dev_pid)) {
+		if ((vendor_id == 0x0 || vendor_id == dev_vid)
+			&& (product_id == 0x0 || product_id == dev_pid)) {
 			struct hid_device_info *tmp;
 
 			/* VID/PID match. Create the record. */
