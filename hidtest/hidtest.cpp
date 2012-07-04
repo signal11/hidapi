1859e00935ec708cd179e0169f8b83a474c934d1
Alan Ott <alan@signal11.us>
2012-07-04 00:00:06 -0400

    Fixes for mac

--- hidtest/hidtest.cpp
+++ hidtest/hidtest.cpp
@@ -66,7 +66,7 @@ int main(int argc, char* argv[])
 	// Open the device using the VID, PID,
 	// and optionally the Serial number.
 	////handle = hid_open(0x4d8, 0x3f, L"12345");
-	handle = hid_open(0x4d8, 0x3f, NULL);
+	handle = hid_open(0x46d, 0xc216, NULL);
 	if (!handle) {
 		printf("unable to open device\n");
  		return 1;
