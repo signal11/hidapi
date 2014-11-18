53f970f1519aa9efe99d743c9c0e70bcf80aa506
amullins <austin.mullins@gmail.com>
2014-11-18 13:18:46 -0600

    Correct comment header for hid_write_timeout() in hidapi.h

--- hidapi/hidapi.h
+++ hidapi/hidapi.h
@@ -179,14 +179,14 @@ extern "C" {
 		The first byte of @p data[] must contain the Report ID. For
 		devices which only support a single report, this must be set
 		to 0x0. The remaining bytes contain the report data. Since
-		the Report ID is mandatory, calls to hid_write() will always
+		the Report ID is mandatory, calls to hid_write_timeout() will always
 		contain one more byte than the report contains. For example,
 		if a hid report is 16 bytes long, 17 bytes must be passed to
-		hid_write(), the Report ID (or 0x0, for devices with a
+		hid_write_timeout(), the Report ID (or 0x0, for devices with a
 		single report), followed by the report data (16 bytes). In
 		this example, the length passed in would be 17.
 
-		hid_write() will send the data on the first OUT endpoint, if
+		hid_write_timeout() will send the data on the first OUT endpoint, if
 		one exists. If it does not, it will send the data through
 		the Control Endpoint (Endpoint 0).
 
